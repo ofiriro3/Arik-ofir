c267990e99a8b6f2fe2ee055fa9359c3f7e90e7f
Your Name <you@example.com>
2016-05-19 22:51:42 +0300

    radix_sort

--- src/homework6/RadixSort.java
+++ src/homework6/RadixSort.java
@@ -2,10 +2,59 @@ package homework6;
 
 public class RadixSort implements Sorter {
 
+	/**
+	 * Since out numbers have only 10 digits perform radix sort over thier 10
+	 * digits 
+	 */
 	@Override
 	public void sort(double[] ar) {
-		// TODO Auto-generated method stub
+
+		for (int i = 0; i < ar.length; i++) {
+			
+			RadixSort(ar , 10-i);
+		}
 		
 	}
 
-}
+	private void RadixSort(double[] ar, int digitToSort) {
+		
+		// Create new array for the counting sort of the specific digit
+		int [] CountingSortCells = new int [10];
+		int [] digitsOfAr = new int [ar.length];
+		
+		for (int i = 0; i < ar.length; i++) {
+			
+			int digit = findingDigit(ar[i], digitToSort);
+			CountingSortCells[digit]++;
+			digitsOfAr[i] = digit;
+		}
+		
+		for (int i = 1; i < CountingSortCells.length; i++) {
+			
+			CountingSortCells[i]+=CountingSortCells[i-1];
+		}
+		double [] newAr = new double [ar.length];
+		for (int i = ar.length-1 ; i >= 0; i--) {
+			
+			int digitOfThisNum = digitsOfAr[i];
+			int newCell = CountingSortCells[digitOfThisNum];
+			newAr[newCell-1] = ar[i];
+			CountingSortCells[digitOfThisNum]--;
+			
+		}
+		
+		ar = newAr;
+		
+		
+	}
+
+	private int findingDigit(double num, int digitToSort) {
+		
+		for (int j = 0; j < digitToSort; j++) {
+			
+			num = num *10 ;
+			
+		}
+		
+		return ((int)num) %10;
+}}
--- src/homework6/RandomArrayFactoryTest.java
+++ src/homework6/RandomArrayFactoryTest.java
@@ -2,21 +2,50 @@ package homework6;
 
 import static org.junit.Assert.*;
 
+import java.awt.List;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
 
 public class RandomArrayFactoryTest {
 
 	@Test
-	public void test() {
+	public void RandomArrayTest() {
 
 		RandomArrayFactory array = new RandomArrayFactory();
 
 		double[] a = array.getRandomArray(10);
 		double digits = RandomArrayFactory.MAXDIGIT;
 
-		for (int i = 0; i < a.length; i++) {
+	}
+	@Test
+	public void RadixSortTest()
+	{
+		double [] array = { 0.523456889 ,0.323456888 ,0.1234567897 ,
+							0.123456789 ,0.922456784 ,0.1238567863};
+		
+		double[] array2 = array.clone();
+		RadixSort sort = new RadixSort();
+		sort.sort(array);
+		Arrays.sort(array2);
+		
+		for (int i = 0; i < array.length; i++) {
 			
-			System.out.println(a[i]);
+			System.out.print(array[i]+" ");
+			if(i%3 ==0 && i>0)
+				System.out.println();
 		}
 	}
+	@Test
+	public void FindDigit()
+	{
+		
+		int i= RadixSort.this.findingDigit(0.123456789, 10);
+				}
+	
+	
+	
 }
