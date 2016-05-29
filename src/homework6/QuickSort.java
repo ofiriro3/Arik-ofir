c5ef135cbde56791d35274ef3eca6bf3aca72aa5
Arik Muller <muller.arik@gmail.com>
2016-05-29 14:10:59 +0300

    First implementation of quickSort

--- src/homework6/QuickSort.java
+++ src/homework6/QuickSort.java
@@ -1,10 +1,43 @@
 package homework6;
 
-public class QuickSort implements Sorter{
+class QuickSort implements Sorter{
 
 	@Override
 	public void sort(double[] ar) {
-		// TODO Auto-generated method stub
+		quickSort(ar, 0, ar.length - 1);
+	}
+
+	private void quickSort(double[] ar, int p, int r){
+		if (p < r){
+			int q = partition(ar, p, r);
+			quickSort(ar, p, q - 1);
+			quickSort(ar, q + 1, r);
+		}
+	}
+	private int partition(double[] ar, int p, int r){
+		double x = ar[p];
+		int i = p - 1;
+		int j = r + 1;
+
+		while (true){
+			do {
+				i++;
+			} while(ar[i] < x);
+
+			do {
+				j--;
+			} while(ar[j] > x);
+			if(i < j)
+				swap(ar, i, j);
+			else
+				return j + 1;
+		}
+
+	}
+	private void swap(double[] ar, int i, int j) {
+		double tmp = ar[j];
+		ar[j] = ar[i];
+		ar[i] = tmp;
 		
 	}
 
