eb8a0491a9723effc999876011f178f686247eca
Arik Muller <muller.arik@gmail.com>
2016-05-29 15:12:18 +0300

    First implementation of randSelect

--- src/homework6/RandSelect.java
+++ src/homework6/RandSelect.java
@@ -1,11 +1,43 @@
 package homework6;
 
-public class RandSelect implements Selector {
+class RandSelect implements Selector {
 
 	@Override
 	public double select(double[] ar, int i) {
-		// TODO Auto-generated method stub
-		return 0;
+		return randSelect(ar, 0, ar.length - 1, i);
 	}
 
+    private double randSelect(double[] ar, int p, int r, int i){
+        if (p == r)
+            return ar[p];
+        int q = randPartition(ar, p ,r);
+        int k = q - p + 1;
+        if (i == k)
+            return ar[q];
+        if (i < k)
+            return randSelect(ar, p, q-1, i);
+        else
+            return randSelect(ar, q+1, r, i-k);
+    }
+
+    private int randPartition(double[] ar, int p, int r) {
+        double x = ar[p];
+        swapRand(ar, p, r);
+        int i = p - 1;
+
+        for (int j = p; j <= r-1; j++) {
+            if (ar[j] <= x) {
+                i++;
+                swapRand(ar, i, j);
+            }
+        }
+        swapRand(ar, i+1, r);
+        return i+1;
+    }
+
+    private void swapRand(double[] ar, int i, int j) {
+        double tmp = ar[i];
+        ar[i] = ar[j];
+        ar[j] = tmp;
+    }
 }
